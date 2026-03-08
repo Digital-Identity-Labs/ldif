9c7285485a0f5ab2d8c3deb46b08b6acdbed2a2a
Pete Birkinshaw <pete@binary-ape.org>
2026-03-08 14:22:26 +0000

    Record and Transform tests

--- test/ldif/import_record_test.exs
+++ test/ldif/import_record_test.exs
@@ -3,16 +3,129 @@ defmodule ImportRecordTest do
 
   alias LDIF.Import.Record
 
-#  describe "entries/2" do
-#    test "returns Not Implemented Yet" do
-#      assert "Not implemented yet" = Export.entries([], [])
-#    end
-#  end
-#
-#  describe "changes/2" do
-#    test "returns Not Implemented Yet" do
-#      assert "Not implemented yet" = Export.changes([], [])
-#    end
-#  end
+  describe "stream/1" do
+
+    test "takes LDIF text and produces a stream of text records, one for each entry" do
+
+      text =
+        """
+        version: 1
+        dn: ou=Product Development, dc=airius, dc=com
+        ou: Product Development
+        objectClass: top
+        objectClass: organizationalUnit
+
+        dn: ou=PD Accountants, ou=Product Development, dc=airius, dc=com
+        ou: Product Development Accountants
+        objectClass: top
+        objectClass: organizationalUnit
+
+        dn: ou=Accounting, dc=airius, dc=com
+        ou: Accounting
+        objectClass: top
+        objectClass: organizationalUnit
+
+        dn: ou=Product Testing, dc=airius, dc=com
+        ou: Product Testing
+        objectClass: top
+        objectClass: organizationalUnit
+        """
+
+        assert is_function(Record.stream(text)) or is_struct(Record.stream(text), Stream)
+        assert 4 = Record.stream(text) |> Enum.to_list() |> Enum.count()
+        assert "version: 1\ndn: ou=Product Development, dc=airius, dc=com\nou: Product Development\nobjectClass: top\nobjectClass: organizationalUnit"
+               = Record.stream(text) |> Enum.to_list() |> List.first()
+
+    end
+
+  end
+
+  describe "unfold/2" do
+
+    test "joins together indented/wrapped/folded lines in a block of text for one record" do
+      text1 =
+      """
+      dn:cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com
+      objectclass:top
+      objectclass:person
+      description:Babs is a big sailing fan, and travels extensively in sea
+        rch of perfect sailing conditions.
+        This is an extra sentence.
+      """
+
+      text2 =
+        """
+        dn:cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com
+        objectclass:top
+        objectclass:person
+        description:Babs is a big sailing fan, and travels extensively in search of perfect sailing conditions. This is an extra sentence.
+        """
+
+      assert ^text2 = Record.unfold(text1)
+
+    end
+
+  end
+
+  describe "split/2" do
+
+    test "produces a list of lists for each line in the record" do
+
+      text =
+        """
+        dn: ou=PD Accountants, ou=Product Development, dc=airius, dc=com
+        ou: Product Development Accountants
+        objectClass: top
+        objectClass: organizationalUnit
+        """
+
+      assert [
+               ["dn", "ou=PD Accountants, ou=Product Development, dc=airius, dc=com"],
+               ["ou", "Product Development Accountants"],
+               ["objectClass", "top"],
+               ["objectClass", "organizationalUnit"]
+             ] = Record.split(text)
+
+    end
+
+
+    test "removes leading version line" do
+      text = """
+      version: 1
+      dn: ou=Product Development, dc=airius, dc=com
+      ou: Product Development
+      objectClass: top
+      objectClass: organizationalUnit
+      """
+
+      assert [
+               ["dn", "ou=Product Development, dc=airius, dc=com"],
+               ["ou", "Product Development"],
+               ["objectClass", "top"],
+               ["objectClass", "organizationalUnit"]
+             ] = Record.split(text)
+
+    end
+
+    test "removes lines beginning with a hash" do
+      text = """
+      # This is an example comment
+      dn: ou=Product Development, dc=airius, dc=com
+      ou: Product Development
+      ## This is another comment with two octothorpes
+      objectClass: top
+      objectClass: organizationalUnit
+      """
+
+      assert [
+               ["dn", "ou=Product Development, dc=airius, dc=com"],
+               ["ou", "Product Development"],
+               ["objectClass", "top"],
+               ["objectClass", "organizationalUnit"]
+             ] = Record.split(text)
+    end
+
+  end
+
 
 end
--- test/ldif/import_transform_test.exs
+++ test/ldif/import_transform_test.exs
@@ -3,16 +3,107 @@ defmodule ImportTransformTest do
 
   alias LDIF.Import.Transform
 
-#  describe "entries/2" do
-#    test "returns Not Implemented Yet" do
-#      assert "Not implemented yet" = Export.entries([], [])
-#    end
-#  end
-#
-#  describe "changes/2" do
-#    test "returns Not Implemented Yet" do
-#      assert "Not implemented yet" = Export.changes([], [])
-#    end
-#  end
+  @attributes [
+    {"dn", "cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com"},
+    {"password", "secret"},
+    {"cn", "Barbara Jensen"},
+    {"cn;lang-en", "Babs Jensen"},
+    {"objectclass", "top"},
+    nil,
+    {"objectclass", "person"},
+    nil,
+    {"objectclass", "organizationalPerson"}
+  ]
+
+  @entry %{
+    "dn" => "cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com",
+    "password" => ["secret"],
+    "cn" => ["Barbara Jensen", "Babs Jensen"],
+    "objectclass" => ["top", "person", "organizationalPerson"]
+  }
+
+  describe "attributes/2" do
+
+    test "removes nil values, leaving only attribute key-value tuples" do
+
+      assert [
+               {"dn", "cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com"},
+               {"password", "secret"},
+               {"cn", "Barbara Jensen"},
+               {"cn;lang-en", "Babs Jensen"},
+               {"objectclass", "top"},
+               {"objectclass", "person"},
+               {"objectclass", "organizationalPerson"}
+             ] = Transform.attributes(@attributes)
+
+    end
+
+    test "simplifies attributes with language tags if option is set to false" do
+      assert [
+               {"dn", "cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com"},
+               {"password", "secret"},
+               {"cn", "Barbara Jensen"},
+               {"cn", "Babs Jensen"},
+               {"objectclass", "top"},
+               {"objectclass", "person"},
+               {"objectclass", "organizationalPerson"}
+             ] = Transform.attributes(@attributes, lang_tags: false)
+
+    end
+
+    test "removes attributes if reject option is set" do
+      assert [
+               {"dn", "cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com"},
+               {"cn", "Barbara Jensen"},
+               {"cn;lang-en", "Babs Jensen"},
+               {"objectclass", "top"},
+               {"objectclass", "person"},
+               {"objectclass", "organizationalPerson"}
+             ] = Transform.attributes(@attributes, reject: ["password"])
+
+    end
+
+    test "redacts attributes if option is set" do
+      assert [
+               {"dn", "cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com"},
+               {"password", "********"},
+               {"cn", "Barbara Jensen"},
+               {"cn;lang-en", "Babs Jensen"},
+               {"objectclass", "top"},
+               {"objectclass", "person"},
+               {"objectclass", "organizationalPerson"}
+             ] = Transform.attributes(@attributes, redact: ["password"])
+
+    end
+
+    test "normalises dn if option is set" do
+      assert [
+               {"dn", "cn=barbara jensen,ou=product development,dc=airius,dc=com"},
+               {"password", "secret"},
+               {"cn", "Barbara Jensen"},
+               {"cn;lang-en", "Babs Jensen"},
+               {"objectclass", "top"},
+               {"objectclass", "person"},
+               {"objectclass", "organizationalPerson"}
+             ] = Transform.attributes(@attributes, normalize_dns: true)
+
+    end
+
+  end
+
+  describe "entry/2" do
+
+    test "converts the multivalue attributes in the single_value option to single values" do
+      assert %{
+               "cn" => "Barbara Jensen",
+               "dn" => "cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com",
+               "objectclass" => ["top", "person", "organizationalPerson"],
+               "password" => ["secret"]
+             } = Transform.entry(@entry, single_value: ["cn"])
+    end
+
+
+
+  end
 
 end
