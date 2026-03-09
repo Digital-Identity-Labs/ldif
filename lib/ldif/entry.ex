42bdac6b872c22c03b493f303f0db5de5eb2282e
Pete Birkinshaw <pete@binary-ape.org>
2026-03-09 12:58:59 +0000

    Entry and tests done, for now

--- lib/ldif/entry.ex
+++ lib/ldif/entry.ex
@@ -13,7 +13,7 @@ defmodule LDIF.Entry do
     attributes: %{}
   ]
 
-  def new(dn, attributes) do
+  def new(dn, attributes \\ %{}) do
     attributes = Map.delete(attributes, "dn")
     %Entry{dn: dn, attributes: attributes}
   end
@@ -22,9 +22,18 @@ defmodule LDIF.Entry do
     entry.dn
   end
 
+  def ndn(entry) do
+    DNUtils.normalize_dn(entry.dn)
+  end
+
   def rdn(entry) do
     DNUtils.rdn(entry.dn)
   end
+
+  def nrdn(entry) do
+    DNUtils.nrdn(entry.dn)
+  end
+
 
   def superior(entry) do
     DNUtils.superior(entry.dn)
@@ -35,16 +44,33 @@ defmodule LDIF.Entry do
   end
 
   def attribute(entry, attribute) do
-    attributes(entry)[attribute] || [] 
+    attributes(entry)[attribute] || []
   end
 
   def get(entry, attribute) do
-    attributes(entry)[attribute] 
+    attributes(entry)[attribute]
   end
-  
+
   def add(entry, attribute, values) do
     old = Map.get(entry.attributes, attribute, [])
     %{entry | attributes: Map.put(entry.attributes, attribute, normlist(old ++ List.wrap(values)))}
+  end
+
+  def merge_tagged(entry) do
+    attributes = entry.attributes
+                 |> Enum.map(fn {k, v} -> {List.first(String.split(k, ";")), v} end)
+                 |> Enum.group_by(fn {n, _v} -> n end, fn {_n, v} -> v end)
+                 |> Enum.map(
+                      fn {k, v} ->
+                        {
+                          k,
+                          List.flatten(v)
+                          |> Enum.uniq()
+                        } end
+                    )
+                 |> Map.new()
+
+    %Entry{entry | attributes: attributes}
   end
 
   def moddn(entry, dn) do
@@ -60,13 +86,33 @@ defmodule LDIF.Entry do
     %{entry | attributes: Map.delete(entry.attributes, attribute)}
   end
 
+  def delete(entry, attribute, value) do
+    values = Map.get(entry.attributes, attribute, [])
+             |> Enum.map(fn v -> if v == value, do: nil, else: v end)
+             |> Enum.uniq()
+             |> Enum.reject(&is_nil/1)
+
+    %{entry | attributes: Map.put(entry.attributes, attribute, values)}
+  end
+
   def replace(entry, attribute, values) do
+    %{entry | attributes: Map.put(entry.attributes, attribute, values)}
+  end
+
+  def replace(entry, attribute, old_value, new_value) do
+    values = Map.get(entry.attributes, attribute, [])
+             |> Enum.map(fn v -> if v == old_value, do: new_value, else: v end)
+             |> Enum.reject(&is_nil/1)
+             |> Enum.uniq()
+
     %{entry | attributes: Map.put(entry.attributes, attribute, values)}
   end
 
   def to_map(entry, opts \\ []) do
     AttrMapper.entry_to_map(entry, opts)
   end
+
+  #################
 
   defp normlist(list) do
     list
--- test/ldif/entry_test.exs
+++ test/ldif/entry_test.exs
@@ -1,18 +1,260 @@
 defmodule EntryTest do
   use ExUnit.Case
 
-  alias LDIF.Apply
-#
-#  describe "new/2" do
-#    test "returns struct" do
-#      assert "cccc" = Apply.new("", %{})
-#    end
-#  end
-#
-#  describe "apply/2" do
-#    test "returns struct" do
-#      assert "cccc" = Apply.apply(change, entry)
-#    end
-#  end
+  alias LDIF.Entry
+
+  @text """
+  dn:cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com
+  objectclass:top
+  objectclass:person
+  objectclass:organizationalPerson
+  cn:Barbara Jensen
+  cn:Barbara J Jensen
+  cn:Babs Jensen
+  sn:Jensen
+  uid:bjensen
+  telephonenumber:+1 408 555 1212
+  description:Babs is a big sailing fan, and travels extensively in sea
+   rch of perfect sailing conditions.
+  title:Product Manager, Rod and Reel Division
+  """
+  @entry  LDIF.decode_entries!(@text)
+          |> List.first()
+
+
+  describe "new/2" do
+
+    test "can be created by passing a DN" do
+      assert %Entry{dn: "cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com", attributes: %{}} =
+               Entry.new("cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com")
+    end
+
+    test "can be passed a simple binary-keyed map for attributes" do
+      assert %Entry{
+               dn: "cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com",
+               attributes: %{
+                 "ou" => ["top", "person"],
+                 "cn" => ["Barbara Jensen"]
+               }
+             } =
+               Entry.new(
+                 "cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com",
+                 %{
+                   "ou" => ["top", "person"],
+                   "cn" => ["Barbara Jensen"]
+                 }
+               )
+    end
+
+  end
+
+  describe "dn/1" do
+
+    test "returns the DN of the entry" do
+      assert "cn=Barbara Jensen, ou=Product Development, dc=airius, dc=com" = Entry.dn(@entry)
+    end
+
+  end
+
+  describe "ndn/1" do
+    test "returns a normalized version of the DN of the entry" do
+      assert "cn=barbara jensen,ou=product development,dc=airius,dc=com" = Entry.ndn(@entry)
+    end
+  end
+
+  describe "rdn/1" do
+    test "returns the RDN of the entry" do
+      assert "cn=Barbara Jensen" = Entry.rdn(@entry)
+    end
+  end
+
+  describe "nrdn/1" do
+    test "returns the normalized RDN of the entry" do
+      assert "cn=barbara jensen" = Entry.nrdn(@entry)
+    end
+  end
+
+  describe "superior/1" do
+    test "returns the superior DN of the entry" do
+      assert "ou=Product Development, dc=airius, dc=com" = Entry.superior(@entry)
+    end
+  end
+
+  describe "attributes/1" do
+    test "returns the attribute map for the entry" do
+      assert %{
+               "cn" => ["Barbara Jensen", "Barbara J Jensen", "Babs Jensen"],
+               "description" => [
+                 "Babs is a big sailing fan, and travels extensively in search of perfect sailing conditions."
+               ],
+               "objectclass" => ["top", "person", "organizationalPerson"],
+               "sn" => ["Jensen"],
+               "telephonenumber" => ["+1 408 555 1212"],
+               "title" => ["Product Manager, Rod and Reel Division"],
+               "uid" => ["bjensen"]
+             } = Entry.attributes(@entry)
+    end
+  end
+
+  describe "attribute/2" do
+    test "returns the values of the specified attribute, or an empty list" do
+      assert ["top", "person", "organizationalPerson"] = Entry.attribute(@entry, "objectclass")
+      assert [] = Entry.attribute(@entry, "password")
+    end
+  end
+
+  describe "get/2" do
+    test "returns the values of the specified attribute, or nil" do
+      assert ["top", "person", "organizationalPerson"] = Entry.get(@entry, "objectclass")
+      assert is_nil(Entry.get(@entry, "password"))
+    end
+  end
+
+  describe "merge_tagged/1" do
+
+    test "merges together tagged attributes, stripping off the tags" do
+      text = """
+      dn:: b3U95Za25qWt6YOoLG89QWlyaXVz
+      objectclass: top
+      objectclass: organizationalUnit
+      ou:: 5Za25qWt6YOo
+      ou;lang-ja:: 5Za25qWt6YOo
+      ou;lang-ja;phonetic:: 44GI44GE44GO44KH44GG44G2
+      ou;lang-en: Sales
+      description: Japanese office
+      """
+      entry = LDIF.decode_entries!(text)
+              |> List.first()
+
+      assert %Entry{
+               dn: "ou=営業部,o=Airius",
+               attributes: %{
+                 "description" => ["Japanese office"],
+                 "objectclass" => ["top", "organizationalUnit"],
+                 "ou" => ["営業部", "Sales", "えいぎょうぶ"]
+               }
+             } = Entry.merge_tagged(entry)
+
+    end
+
+  end
+
+  describe "add/3" do
+
+    test "adds a value to the named attribute" do
+      assert %Entry{
+               attributes: %{
+                 "objectclass" => ["top", "person", "organizationalPerson", "testPerson"]
+               }
+             } = Entry.add(@entry, "objectclass", "testPerson")
+    end
+
+    test "creates the attribute if it does not already exist" do
+      assert %Entry{
+               attributes: %{
+                 "testStatus" => ["good"]
+               }
+             } = Entry.add(@entry, "testStatus", "good")
+    end
+
+  end
+
+  describe "moddn/2" do
+
+    test "changes the DN of the entry" do
+      assert %Entry{
+               dn: "cn=Barbara Example Jensen, ou=Product Development, dc=airius, dc=com"
+             } = Entry.moddn(@entry, "cn=Barbara Example Jensen, ou=Product Development, dc=airius, dc=com")
+    end
+
+  end
+
+  describe "modrdn/2" do
+
+    test "changes the RDN of the entry" do
+      assert %Entry{
+               dn: "cn=Babs Example Jensen, ou=Product Development, dc=airius, dc=com"
+             } = Entry.modrdn(@entry, "cn=Babs Example Jensen")
+    end
+
+  end
+
+  describe "delete/2" do
+
+    test "deletes an attribute completely" do
+      assert [
+               "cn",
+               "description",
+               "objectclass",
+               "sn",
+               "title",
+               "uid"
+             ] = Map.get(
+                   Entry.delete(@entry, "telephonenumber"),
+                   :attributes
+                 )
+                 |> Map.keys()
+    end
+
+  end
+
+  describe "delete/3" do
+
+    test "deletes one value from an attribute" do
+      assert ["top", "person"] = Entry.delete(@entry, "objectclass", "organizationalPerson")
+                                 |> Map.get(:attributes, %{})
+                                 |> Map.get("objectclass")
+    end
+
+  end
+
+  describe "replace/3" do
+
+    test "replaces values for an attribute with another list of values" do
+      assert ["bird", "pigeon"] = Entry.replace(@entry, "objectclass", ["bird", "pigeon"])
+                                  |> Map.get(:attributes, %{})
+                                  |> Map.get("objectclass")
+    end
+
+  end
+
+  describe "replace/4" do
+
+    test "replaces a value for an attribute with another one" do
+      assert ["top", "person", "examplePerson"] = Entry.replace(
+                                                    @entry,
+                                                    "objectclass",
+                                                    "organizationalPerson",
+                                                    "examplePerson"
+                                                  )
+                                                  |> Map.get(:attributes, %{})
+                                                  |> Map.get("objectclass")
+    end
+
+  end
+
+  describe "to_map/2" do
+
+    test "converts an entry struct to a simple map" do
+      assert %{} = Entry.to_map(@entry)
+    end
+
+    test "the map has snake_cased atom keys" do
+      assert [
+               :description,
+               :title,
+               :cn,
+               :dn,
+               :object_class,
+               :surname,
+               :telephone_number,
+               :username
+             ] = Entry.to_map(
+                   @entry
+                 )
+                 |> Map.keys()
+    end
+
+  end
 
 end
